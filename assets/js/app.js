14d80f7f0764a21ec73e285e03f9ea46a574d681
Anthony Kim <akim2214@gmail.com>
2018-01-25 17:04:45 -0800

    updated all files with functional code composed by team

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,32 @@
 // $(document).ready(function () {
+var config = {
+    apiKey: "AIzaSyDORz1e9e4aHUhPBHLbtxoBsE5z6DT5980",
+    authDomain: "eat-in-c8063.firebaseapp.com",
+    databaseURL: "https://eat-in-c8063.firebaseio.com",
+    projectId: "eat-in-c8063",
+    storageBucket: "eat-in-c8063.appspot.com",
+    messagingSenderId: "1078349257986"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+//---------------------------------------------------------------------
+
 var listIngredients = ["onion", "garlic", "carrot", "celery", "broccoli",
     "cauliflower", "potato", "sweet potato", "red onion", "bell pepper",
     "shallot", "eggplant", "zucchini", "chicken", "beef",
-    "pork", "shrimp", "salmon", "tilapia", "butter", "milk", "cheddar", "cheese"];
+    "pork", "shrimp", "salmon", "tilapia", "butter", "milk", "cheddar", "cheese",
+    "egg", "salt", "pepper", "parsley"
+];
 var searchIngredients = [];
 var difficulty = ["Easy", "Medium", "Hard"];
 var time = ["HH:mm"];
 var apiKey = "2b8eb696ebf1b4d8ff30c5e5d4e49b39";
 var appID = "dbd3948d";
 
-
 var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=" + appID + "&_app_key=" + apiKey + "&your_search_parameters";
 
+//-----------------------------------------------------------------
 
 function renderButtons() {
     $("#ingredientButton").empty();
@@ -24,55 +39,91 @@ function renderButtons() {
     }
 };
 renderButtons();
-$("button").css({ "margin-right": "5px" });
-
+// $("button").css({"margin-right": "5px"});
 
 $(document).on("click", "#add", function () {
+    event.preventDefault();
     if ($("#add").val().trim() == "") {
         alert("Please type ingredient to add");
-    }
-    else {
+    } else {
         var userEntry = $("#addEntry").val().trim();
         listIngredients.push(userEntry);
         $("#addEntry").val("");
         renderButtons();
-        $("button").css({ "margin": "5px", "padding": "5px" });
-        console.log("user entry:" + userEntry);
-        console.log("array:" + listIngredients);
         return false;
-        //enter key should submit and render buttons too...
     }
 });
 
 $(document).on("click", ".ingredient-button", function () {
+
+    var addedIngredient = $("<button>");
+    addedIngredient.addClass("selected-ingredient-button");
+    addedIngredient.text($(this).attr("data-name"));
+    $("#ingredientList").append(addedIngredient);
     ingredient = $(this).text();
     searchIngredients.push(ingredient);
-    listIngredients.splice(ingredient);
-    function renderSearchIngredientButton() {
-        $("#selectedIngredients").empty();
-        for (var j = 0; j < searchIngredients.length; j++) {
-            var b = $("<button>");
-            b.addClass("selected-ingredient-button");
-            b.attr("data-name", searchIngredients[i]);
-            b.text(searchIngredients[i]);
-            $("#selectedIngredients").append(b);
-            console.log(b);
-        }
-    };
-    console.log(ingredient);
     console.log(searchIngredients);
+
 });
+
 
 $(document).on("click", ".selected-ingredient-button", function () {
     ingredient = $(this).val();
     listIngredients.push(ingredient);
-    searchIngredients.splice(ingredient);
-    renderButtons();
+    // searchIngredients.splice(4, 2);
+
+    // renderButtons();
 });
 
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    //   })
+$(document).on("click", ".clear-button", function () {
+    $("#ingredientList").empty();
+    searchIngredients = [];
+});
 
-// })
+
+$(document).on("click", ".find-button", function () {
+    var searchQuery = searchIngredients.join("+");
+    console.log("searchterms:" + searchQuery);
+
+    var apiKey = "2b8eb696ebf1b4d8ff30c5e5d4e49b39";
+    var appID = "dbd3948d";
+    var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=" + appID + "&_app_key=" + apiKey + "&q=" + searchQuery + "&maxResult=10&start=10";
+    console.log("api url:" + queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+        var results = response.matches;
+        console.log(results);
+        for (var j = 0; j < results.length; j++) {
+            var mealImage = results[j].smallImageUrls;
+            var mealName = results[j].recipeName;
+            var mealTime = results[j].totalTimeInSeconds;
+            var mealId = results[j].id;
+            // push data from api call to firebase
+            var recipes = [{
+                name: mealName,
+                image: mealImage,
+                time: mealTime,
+                id: mealId,
+            }];
+            database.ref("recipes").push(recipes);
+
+            //     database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+            //          name = 
+            // })
+        }
+    })
+
+
+
+    // $("#preRecipes").empty();
+    //     // $("#preRecipes").append(mealImage);
+    // mealImage.attr("alt", "meal image");
+    //     // var displayMealName = $("<p>").html(mealName);
+    //     var displayMealImage = $("<img>").attr("src", mealImage);
+    //     // append
+    //     // var displayMealTime = $("<p>").html(mealTime);
+    //     // append
+});
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -11,22 +11,57 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 
-$(document).on("click", "#search", function () {
+$(document).on("click", ".find-button", function () {
     // var searchIngredients = array joined by "+" or "%20";
     // searchIngredients.toString();
-    // var searchIngredients = str.replace(", ", "+");
+    // var searchIngredients = str.replace(",", "+");
     var apiKey = "2b8eb696ebf1b4d8ff30c5e5d4e49b39";
     var appID = "dbd3948d";
-    var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=" + appID + "&_app_key=" + apiKey + "&q=" + ingredientSelection + "&maxResult=10&start=10";
+    var queryURL = "http://api.yummly.com/v1/api/recipe/" + recipeId + "?_app_id=" + appID + "&_app_key=" + apiKey;
+    var recipeId;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).done(function (response) {
-        var results = response.data;
-        // push data from api call to firebase
+        var results = response.matches;
+        console.log(results);
+        for (var j = 0; j < results.length; j++) {
+            var mealImage = results[j].smallImageUrls;
+            var mealName = results[j].recipeName;
+            var mealTime = results[j].totalTimeInSeconds;
+            var mealId = results[j].id;
+            // push data from api call to firebase
+            var recipes = [{
+                name: mealName,
+                image: mealImage,
+                time: mealTime,
+                id: mealId,
+            }];
+            database.ref().push(recipes);
+            //     database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+            //          name = 
+            // })
+        }
     })
     
+    database.ref("recipes").on("child_added", function(childSnapshot, prevChildKey) {
+
+        // console.log(childSnapShot.val());
+        
+        var mealImage = childSnapshot.val().image;
+        var mealName = childSnapshot.val().name;
+        var mealTime = childSnapshot.val().time;
+        var mealId = childSnapshot.val().id;
+        var getRecipe = ("<button value = mealId>");
+
+
+        // var currentTime = moment();
+        var convertTime = moment(mealTime);
+        
+       
+        $("#recipeTable > tbody").append("<tr><td><img src=" + mealImage + "></td><td>" + mealName + "</td><td>" + convertTime + "</td><td><button id='getrecipe' value=" + mealId + "></button></td></tr>");
+
     // $("#preRecipes").empty();
     // for (var j = 0; j < results.length; j++) {
     //     var mealImage = results[j].smallImageUrls;
@@ -40,5 +75,3 @@ $(document).on("click", "#search", function () {
     //     // var displayMealTime = $("<p>").html(mealTime);
     //     // append
     // }
-
-});
